43c2e8d9b0d3d74fb20d9a6b93ce0f51563203e5
Xanderath <alexgiller@proton.me>
2026-04-28 22:59:32 -0400

    use current time instad durration from start for log messages

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use serenity::all::Interaction;
 use tokio;
 use notify::{self, Event, EventKind, Watcher, event};
 use serde::Deserialize;
-use tokio::time::Instant;
+// use tokio::time::Instant;
 use std::env;
 // use serde_json::Value;
 // use std::io::Read;
@@ -238,7 +238,7 @@ struct Config{
 }
 
 fn start_auto_update(s3s_path:Option<String>,nxapi_path:Option<String>,s3s_config_path:Option<String>,update_minutes:u64,s3s_run_path:PathBuf){
-    let start_time=Instant::now();
+    // let start_time=Instant::now();
     let updating_tokens_lock=Arc::new(TokioMutex::new(()));
     // spawn game updater
     if let Some(s3s_path) =s3s_path 
@@ -258,13 +258,14 @@ fn start_auto_update(s3s_path:Option<String>,nxapi_path:Option<String>,s3s_confi
                 //update games
                 update_games_interval.tick().await;
                 let lock=updating_tokens_lock.lock().await;
-                println!("updating games at {} secs since start ...",(Instant::now()-start_time).as_secs());
+                println!("updating token at {} ...",chrono::Local::now());
                 let child=tokio::process::Command::new("python3")
                 .args(vec!(&s3s_path.to_string_lossy(),"-o"))
                 .current_dir(&s3s_run_path)
                 .spawn();
                 if let Ok(mut child)=child{
                     if let Err(_)= tokio::time::timeout(Duration::from_mins(15), child.wait()).await{
+                        println!("Killing games update at {}",chrono::Local::now());
                         let _=child.kill().await;
                     }
                 }
@@ -282,12 +283,13 @@ fn start_auto_update(s3s_path:Option<String>,nxapi_path:Option<String>,s3s_confi
             loop{
                 update_games_interval.tick().await;
                 let lock=updating_tokens_lock.lock().await;
-                println!("updating token at {} secs since start ...",(Instant::now()-start_time).as_secs());
+                println!("updating token at {} ...",chrono::Local::now());
                 let child =tokio::process::Command::new(&nxapi_path)
                 .args(vec!("util","update-s3s-token",&s3s_config_path))
                 .spawn();
                 if let Ok(mut child)=child{
                     if let Err(_)= tokio::time::timeout(Duration::from_mins(5), child.wait()).await{
+                        println!("Killing token update at {}",chrono::Local::now());
                         let _=child.kill().await;
                     }
                 }
